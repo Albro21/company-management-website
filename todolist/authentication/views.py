cce08c0dcdbc56ae91b78dd5d5fbf67ca50f32f1
Albert <albert.kurto@gmail.com>
2025-01-19 11:06:01 +0200

    Registration page created and works

--- todolist/authentication/views.py
+++ todolist/authentication/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def register(request):
-    return render(request, 'registration/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html' , {'form': form})
