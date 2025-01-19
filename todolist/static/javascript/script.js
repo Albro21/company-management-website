6ae0ebf245931c4c530567b9673ba25c197d9daf
sarcoma29 <soroin.georiy@yandex.ru>
2025-01-19 16:14:37 +0300

    note form

---
+++ todolist/static/javascript/script.js
@@ -0,0 +1,10 @@
+function showWindow(){
+    var el = document.getElementById("content_window");
+    el.style.display = 'block';
+  }
+
+function closeWindow(){
+    var el = document.getElementById("content_window");
+    el.style.display = 'none';
+  }
+  
