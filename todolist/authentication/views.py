54fd973834acd07d88aa87fe6bc116332a96b718
Albert <albert.kurto@gmail.com>
2025-01-19 23:04:40 +0200

    feat: add required fields to the registration form
    - Made email, first_name, and last_name fields required and added to the registration form

--- todolist/authentication/views.py
+++ todolist/authentication/views.py
@@ -1,14 +1,14 @@
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Registration successful! You can now log in.')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html' , {'form': form})
