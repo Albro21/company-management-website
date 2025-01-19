e1f30daab2a6be4efde3f9341411c396759be69a
Albert <albert.kurto@gmail.com>
2025-01-19 09:28:15 +0200

    fix login required

--- todolist/main/views.py
+++ todolist/main/views.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 from django.shortcuts import render 
 from .models import Project, Category, Task
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     user = request.user
     projects = Project.objects.filter(user=user)
