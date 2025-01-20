4055deb76f496d9e354912123e9adc8e483944d0
sarcoma29 <soroin.georiy@yandex.ru>
2025-01-20 19:24:30 +0300

    note form update

--- todolist/static/javascript/script.js
+++ todolist/static/javascript/script.js
@@ -1,10 +1,27 @@
 function showWindow(){
-    var el = document.getElementById("content_window");
+    let el = document.getElementById("content_window");
     el.style.display = 'block';
   }
 
 function closeWindow(){
-    var el = document.getElementById("content_window");
+    let el = document.getElementById("content_window");
     el.style.display = 'none';
   }
+
+
+
+
+
+/* проверка на возвращаемую дату
+function WriteDate() {
+  let dateControl = document.querySelector('input[type="date"]');
+  dateControl;
+  console.log(dateControl.value);
+  console.log(dateControl.valueAsNumber);
+}
+*/
+
+
+
+
   
