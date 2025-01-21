daeb7a2836462e8445142f989fb11baf709f7b37
Albert <albert.kurto@gmail.com>
2025-01-21 22:44:49 +0200

    Feat: Form for creating tasks is now working

--- todolist/main/views.py
+++ todolist/main/views.py
@@ -1,20 +1,34 @@
 from collections import defaultdict
-from django.shortcuts import render 
-from .models import Project, Category, Task
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Project, Category, Task
+from .forms import TaskCreationForm
 
-# Create your views here.
+
 @login_required
 def index(request):
     user = request.user
+    
     projects = Project.objects.filter(user=user)
-    categories = Category.objects.all().filter(user=user)
-    tasks = Task.objects.all().filter(user=user)
+    categories = Category.objects.filter(user=user)
+    tasks = Task.objects.filter(user=user)
+    
+    if request.method == 'POST':
+        form = TaskCreationForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = user
+            task.save()
+            form.save_m2m()
+            return redirect('index')
+    else:
+        form = TaskCreationForm()
 
     context = {
         'projects': projects,
         'categories': categories,
-        'tasks': tasks
+        'tasks': tasks,
+        'form': form,
     }
-    
+
     return render(request, 'main/index.html', context)
