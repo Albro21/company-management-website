fd4b551ca4a977eb3eb149e09a28e754158c6e79
Albert <albert.kurto@gmail.com>
2025-02-08 22:50:39 +0200

    Moved everything about Profile model into authentication app

--- todolist/authentication/models.py
+++ todolist/authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg')
+    
+    class Meta:
+        app_label = 'auth'
+    
+    def __str__(self):
+        return f"{self.user.username} Profile"
--- todolist/main/models.py
+++ todolist/main/models.py
@@ -64,14 +64,3 @@ class Task(models.Model):
     def toggle_priority(self):
         self.is_high_priority = not self.is_high_priority
         self.save()
-        
-        
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg')
-    
-    class Meta:
-        app_label = 'auth'
-    
-    def __str__(self):
-        return f"{self.user.username} Profile"
