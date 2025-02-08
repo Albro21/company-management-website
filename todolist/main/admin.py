f0837cbb8dae012a84a197154af13d1c91333dc0
Albert <albert.kurto@gmail.com>
2025-02-08 22:44:09 +0200

    Added profile model to admin.py

--- todolist/main/admin.py
+++ todolist/main/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Project, Category, Task
+from django.contrib.auth.models import User
+from .models import Profile
 
 
 admin.site.register(Project)
 admin.site.register(Category)
 admin.site.register(Task)
+
+
+# Custom user admin
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profile"
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
--- todolist/main/models.py
+++ todolist/main/models.py
@@ -70,5 +70,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg')
     
+    class Meta:
+        app_label = 'auth'
+    
     def __str__(self):
         return f"{self.user.username} Profile"
