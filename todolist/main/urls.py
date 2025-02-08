a9259a921d9458f015e55a35a746e0b581f18913
Albert <albert.kurto@gmail.com>
2025-02-08 22:17:58 +0200

    Added media folder and media settings in settings.py

--- todolist/main/urls.py
+++ todolist/main/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -10,3 +12,6 @@ urlpatterns = [
     path('project/<int:project_id>/', views.project_detail, name='project_detail'),
     path('profile', views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
